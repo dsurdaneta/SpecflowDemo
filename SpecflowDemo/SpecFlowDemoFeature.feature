21254c83a50ac40ff56d209cec249e70febe8bb9
Daniel Urdaneta <mangafever@gmail.com>
2016-12-13 13:03:42 +0100

    First demo commit

---
+++ SpecflowDemo/SpecFlowDemoFeature.feature
@@ -0,0 +1,24 @@
+﻿Feature: SpecFlowDemoFeature
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Demo001_Add two numbers
+	Given I have entered 50 into the calculator
+	And I have also entered 70 into the calculator
+	When I press add
+	Then the result should be 120 on the screen
+
+@mytag
+Scenario Outline: Demo002_Add two numbers Outline
+	Given I have entered <FirstNumber> into the calculator
+	And I have also entered <SecondNumber> into the calculator
+	When I press add
+	Then the result should be <ResultNumber> on the screen
+Examples: 
+	| FirstNumber | SecondNumber | ResultNumber |
+	| 7			  | 8			 | 15			|
+	| 500		  | 1250		 | 1750			|
+	| 39		  | 2			 | 41			|
+	
---
+++ SpecflowDemo/SpecFlowDemoSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace SpecflowDemo
+{
+    [Binding]
+    public sealed class SpecFlowDemoSteps
+    {
+        // For additional details on SpecFlow step definitions see http://go.specflow.org/doc-stepdef
+
+        [Given("I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredSomethingIntoTheCalculator(int firstNumber)
+        {
+            ScenarioContext.Current.Add("firstNumber", firstNumber);
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int secondNumber)
+        {
+            ScenarioContext.Current.Add("secondNumber", secondNumber);
+        }
+
+        [When("I press add")]
+        public void WhenIPressAdd()
+        {
+            int firstNumber = ScenarioContext.Current.Get<int>("firstNumber");
+            int secondNumber = ScenarioContext.Current.Get<int>("secondNumber");
+
+            ScenarioContext.Current.Add("expectedResultNumber", firstNumber + secondNumber);
+        }
+
+        [Then("the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            int expectedResultNumber = ScenarioContext.Current.Get<int>("expectedResultNumber");
+            Assert.AreEqual(expectedResultNumber, result);
+        }
+    }
+}
